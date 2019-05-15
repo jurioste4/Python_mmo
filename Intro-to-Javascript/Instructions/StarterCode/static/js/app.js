f2200eefa15aa3670a0661f4c75b8429044d299c
jurioste4 <jurioste4@gmail.com>
2019-05-15 00:58:09 -0500

    update

--- Intro-to-Javascript/Instructions/StarterCode/static/js/app.js
+++ Intro-to-Javascript/Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+
+console.log(tableData);
+
+tableData.forEach(function(tableData){
+    console.log(tableData);
+    var row =tbody.append("tr");
+
+    Object.entries(tableData).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    })
+})
